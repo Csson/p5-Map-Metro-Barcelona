a7de7001bf7db203286bfcf043060eab3c598e51
Erik Carlsson <info@code301.com>
2015-02-02 21:34:26 +0100

    Improved synopsis. Cleanup

--- lib/Map/Metro/Plugin/Map/Barcelona.pm
+++ lib/Map/Metro/Plugin/Map/Barcelona.pm
@@ -3,6 +3,7 @@ use 5.14.0;
 package Map::Metro::Plugin::Map::Barcelona;
 
 # VERSION
+# ABSTRACT: Map::Metro map for Barcelona
 
 use Moose;
 with 'Map::Metro::Plugin::Map';
@@ -19,16 +20,22 @@ sub map_package {
 
 1;
 
-# ABSTRACT: Map::Metro map for Barcelona
 
 __END__
 
 =pod
 
+=encoding utf-8
+
 =head1 SYNOPSIS
 
     use Map::Metro;
     my $graph = Map::Metro->new('Barcelona')->parse;
+
+Or:
+
+    $ map-metro.pl route Barcelona Paral·lel Tibidabo
+    $ map-metro.pl route Barcelona Parallel Tibidabo
 
 =head1 DESCRIPTION
 
@@ -36,16 +43,12 @@ See L<Map::Metro> for usage information.
 
 =head1 STATUS
 
-See L<Lines and stations|Map::Metro::Plugin::Map::Barcelona::Lines>
+This map L<contains|Map::Metro::Plugin::Map::Barcelona::Lines>:
 
-As of 2015-jan-04 it contains:
-
+=for :list
 * The eleven L-lines (L1 to L11) [L<wikipedia|https://en.wikipedia.org/wiki/Barcelona_Metro>]
-
 * The I<Tramvia Blau> [L<wikipedia|https://en.wikipedia.org/wiki/Tramvia_Blau>]
-
 * I<Funicular del Tibidabo> [L<wikipedia|https://en.wikipedia.org/wiki/Funicular_del_Tibidabo>]
-
 * I<Funicular de Montjuïc> [L<wikipedia|https://en.wikipedia.org/wiki/Funicular_de_Montju%C3%AFc>]
 
 =head2 Notes
@@ -54,10 +57,6 @@ As of 2015-jan-04 it contains:
 
 * I<Tramvia Blau> only includes the two end-points.
 
-=begin HTML
-
-<p><a href="https://raw.githubusercontent.com/Csson/p5-Map-Metro-Barcelona/master/static/images/barcelona.png"><img src="https://raw.githubusercontent.com/Csson/p5-Map-Metro-Barcelona/master/static/images/barcelona.png" style="max-width: 600px" /></a></p>
-
-=end HTML
+=for HTML <p><a href="https://raw.githubusercontent.com/Csson/p5-Map-Metro-Barcelona/master/static/images/barcelona.png"><img src="https://raw.githubusercontent.com/Csson/p5-Map-Metro-Barcelona/master/static/images/barcelona.png" style="max-width: 600px" /></a></p>
 
 =cut
